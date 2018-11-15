bcb2c623c6944b3df2dbb5f55362eb3117d064a0
liruil <774255050@qq.com>
2018-11-15 12:59:36 +0800

    修复bug 添加主机功能

--- app.py
+++ app.py
@@ -3,13 +3,88 @@ import requests, json, os
 
 app = Flask(__name__)
 
+app.scrapyd_url = ''
+
+
+@app.route('/gxyf/clients', methods=['GET'])
+def clients():
+    try:
+        with open('./conf/conf.json', "r", encoding="utf-8") as f:
+            conf_json = json.load(f)
+            data_json = {'status':'ok', 'data': []}
+            data_json['data'] = conf_json
+            return json.dumps(data_json)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '暂未找到服务器'}
+        return json.dumps(data_json)
+
+
+@app.route('/gxyf/updateConf', methods=['GET'])
+def updateConf():
+    try:
+        name = request.args.get('name')
+        type = request.args.get('type')
+        with open('./conf/conf.json', "r", encoding="utf-8") as f:
+            conf_json = json.load(f)
+            if type == 'add':
+                host = request.args.get('host')
+                port = request.args.get('port')
+                conf_json_obj = {
+                    'title': host + ':' + port,
+                    'host': host,
+                    'port': port,
+                    'name': name
+                }
+                conf_json.append(conf_json_obj)
+            elif type == 'remove':
+                conf_json_new = []
+                for c in conf_json:
+                    if c['name'] != name:
+                        conf_json_obj = {
+                            'title': c['title'],
+                            'host': c['host'],
+                            'port': c['port'],
+                            'name': c['name']
+                        }
+                        conf_json_new.append(conf_json_obj)
+                conf_json = conf_json_new
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '暂未找到服务器'}
+        return json.dumps(data_json)
+    with open('./conf/conf.json', "w", encoding="utf-8") as f:
+        json.dump(conf_json, f, ensure_ascii=False)
+    data_json = {'status':'ok'}
+    return json.dumps(data_json)
+
+@app.route('/gxyf/daemonstatus', methods=['GET'])
+def daemonstatus():
+    host = request.args.get('host')
+    port = request.args.get('port')
+    app.scrapyd_url = "http://"+host+":"+port
+    url = app.scrapyd_url + "/daemonstatus.json"
+    try:
+        resp = requests.get(url)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '连接不上该服务器，正在尝试重新连接，请检查地址是否有误或服务是否开启'}
+        return json.dumps(data_json)
+    data_json = json.loads(resp.text)
+    return json.dumps(data_json)
+
 @app.route('/gxyf/start', methods=['GET'])
 def start():
     project = request.args.get('project')
     spider = request.args.get('spider')
-    url = "http://localhost:6800/schedule.json"
+    url = app.scrapyd_url + "/schedule.json"
     params = {"project": project, "spider": spider}
-    resp = requests.post(url, data=params)
+    try:
+        resp = requests.post(url, data=params)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '服务器连接异常'}
+        return json.dumps(data_json)
     data_json = json.loads(resp.text)
     return json.dumps(data_json)
 
@@ -17,9 +92,14 @@ def start():
 def stop():
     project = request.args.get('project')
     job = request.args.get('job')
-    url = "http://localhost:6800/cancel.json"
+    url = app.scrapyd_url + "/cancel.json"
     params = {"project": project, "job": job}
-    resp = requests.post(url, data=params)
+    try:
+        resp = requests.post(url, data=params)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '服务器连接异常'}
+        return json.dumps(data_json)
     data_json = json.loads(resp.text)
     return json.dumps(data_json)
 
@@ -27,9 +107,14 @@ def stop():
 @app.route('/gxyf/delete', methods=['GET'])
 def delete():
     project = request.args.get('project')
-    url = "http://localhost:6800/delproject.json"
+    url = app.scrapyd_url + "/delproject.json"
     params = {"project": project}
-    resp = requests.post(url, data=params)
+    try:
+        resp = requests.post(url, data=params)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '服务器连接异常'}
+        return json.dumps(data_json)
     data_json = json.loads(resp.text)
     return json.dumps(data_json)
 
@@ -37,11 +122,23 @@ def delete():
 @app.route('/gxyf/status', methods=['GET'])
 def status():
     projects = request.args.get('project')
-    projects = projects.split(',')
     data_array = []
+    if projects == '':
+        noPro = {
+            'msg': '该服务器暂无爬取记录',
+            'status': 'error'
+        }
+        data_array.append(noPro)
+        return json.dumps(data_array)
+    projects = projects.split(',')
     for project in projects:
-        url = "http://localhost:6800/listjobs.json?project="+project
-        resp = requests.get(url)
+        url = app.scrapyd_url + "/listjobs.json?project="+project
+        try:
+            resp = requests.get(url)
+        except Exception as e:
+            print(e)
+            data_json = {'status': 'error', 'msg': '服务器连接异常'}
+            return json.dumps(data_json)
         data_json = json.loads(resp.text)
         data_json = can_status_dict(data_json, project)
         data_array.append(data_json)
@@ -59,9 +156,14 @@ def upload():
         files.save(new_fname)
 
     files = {'egg': (open('eggs/'+project+'.egg', 'rb'))}
-    url = "http://localhost:6800/addversion.json"
+    url = app.scrapyd_url + "/addversion.json"
     params = {"project": project, "version": version}
-    resp = requests.post(url, data=params, files=files)
+    try:
+        resp = requests.post(url, data=params, files=files)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '服务器连接异常'}
+        return json.dumps(data_json)
     data_json = json.loads(resp.text)
     os.remove('eggs/'+project+'.egg')
     return json.dumps(data_json)
@@ -72,22 +174,34 @@ def can_data_list():
     # 初始化 data_dict
     data_dict = {'status': 'ok','data':[]}
     # 列出 scrapyd 部署过的项目
-    pro_list_url = "http://localhost:6800/listprojects.json"
-    pro_data = requests.get(pro_list_url)
+    pro_list_url = app.scrapyd_url + "/listprojects.json"
+    try:
+        pro_data = requests.get(pro_list_url)
+    except Exception as e:
+        print(e)
+        data_json = {'status': 'error', 'msg': '服务器连接异常'}
+        return json.dumps(data_json)
     pro_list = can_data_dict(pro_data, 'projects')
 
-    if pro_list == 'error':
-        return pro_list
+    if len(pro_list) == 0:
+        noProObj = {
+            'msg': '该服务器暂无爬虫项目',
+            'status': 'error'
+        }
+        return json.dumps(noProObj)
     else:
         # 列出项目下的所有爬虫
         for pro_name in pro_list:
             pro_obj = []
-            spi_list_url = "http://localhost:6800/listspiders.json?project=" + pro_name
-            spi_data = requests.get(spi_list_url)
+            spi_list_url = app.scrapyd_url + "/listspiders.json?project=" + pro_name
+            try:
+                spi_data = requests.get(spi_list_url)
+            except Exception as e:
+                print(e)
+                data_json = {'status': 'error', 'msg': '服务器连接异常'}
+                return json.dumps(data_json)
             spi_list = can_data_dict(spi_data, 'spiders')
-            if spi_list == 'error':
-                return spi_list
-            else:
+            if len(spi_list) != 0:
                 for spi_name in spi_list:
                     pro_spi = {}
                     pro_spi['pro_name'] = pro_name
@@ -116,12 +230,12 @@ def can_status_dict(data_json, project):
             data_json[st][i]['project'] = project
             if st == 'running' or st == 'finished':
                 data_json[st][i]['start_time'] = data_json[st][i]['start_time'][0:19]
-                data_json[st][i]['log'] = 'http://localhost:6800/logs/' + data_json[st][i]['project'] + '/' + data_json[st][i]['spider'] + '/' + data_json[st][i]['id'] + '.log'
+                data_json[st][i]['log'] = app.scrapyd_url + '/logs/' + data_json[st][i]['project'] + '/' + data_json[st][i]['spider'] + '/' + data_json[st][i]['id'] + '.log'
             if st == 'finished':
                 data_json[st][i]['end_time'] = data_json[st][i]['end_time'][0:19]
             i += 1
     return data_json
 
 if __name__ == '__main__':
-    app.run()
-    # app.run(debug=True,host='0.0.0.0', port=8080)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001)
